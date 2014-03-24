4ddceed529336e4a40f2d94af32a6af6103a9d3d
Daniel Meiers <daniel.meiers@cismet.de>
2014-03-24 13:05:07 +0100

    removed test cases generated by angular web app generator

--- test/spec/directives/catalogue-tree.js
+++ test/spec/directives/catalogue-tree.js
@@ -2,19 +2,8 @@
 
 describe('Directive: catalogueTree', function () {
 
-  // load the directive's module
-  beforeEach(module('simpleWorldstatetTreeApp'));
+  it('should do something', function () {
+    expect(true).toBe(true);
+  });
 
-  var element,
-    scope;
-
-  beforeEach(inject(function ($rootScope) {
-    scope = $rootScope.$new();
-  }));
-
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<catalogue-tree></catalogue-tree>');
-    element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the catalogueTree directive');
-  }));
 });
--- test/spec/services/nodes.js
+++ test/spec/services/nodes.js
@@ -2,17 +2,8 @@
 
 describe('Service: Nodes', function () {
 
-  // load the service's module
-  beforeEach(module('simpleWorldstatetTreeApp'));
-
-  // instantiate service
-  var Nodes;
-  beforeEach(inject(function (_Nodes_) {
-    Nodes = _Nodes_;
-  }));
-
   it('should do something', function () {
-    expect(!!Nodes).toBe(true);
-  });
+    expect(true).toBe(true);
+   });
 
 });
