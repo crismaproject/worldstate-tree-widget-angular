bdbad2b457694acf1f7e73c1ad0a8913d5a15568
Daniel Meiers <daniel.meiers@cismet.de>
2014-03-27 15:19:40 +0100

    #1 visualising the status of the filter search result

--- app/scripts/directives/catalogue-filter.js
+++ app/scripts/directives/catalogue-filter.js
@@ -1,7 +1,7 @@
 angular.module(
         'de.cismet.crisma.widgets.worldstateTreeWidget.directives',
         [
-             'de.cismet.commons.angular.angularTools',
+            'de.cismet.commons.angular.angularTools',
             'de.cismet.crisma.widgets.worldstateTreeWidget.services'
         ]
         ).directive('catalogueFilter',
@@ -17,15 +17,43 @@ angular.module(
 //                    
 //                },
                     controller: function($scope) {
-                        $scope.$watch('filterExpression', function() {
-                            if($scope.filterExpression){
-                                $scope.filterResult = Nodes.query({
-                                    filter: 'name:.*'+$scope.filterExpression+'.*'
-                                }, function(data) {
-                                    AngularTools.safeApply($scope);
-                                });
+                        $scope.filterClass = {
+                            'has-success': false,
+                            'has-error': false
+                        };
+                        $scope.filterIcon = {
+                            'glyphicon-ok': false,
+                            'glyphicon-warning-sign': false
+                        }
+
+                        $scope.$watch('filterExpression', function(newVal, oldVal) {
+                            if (newVal !== oldVal) {
+                                if ($scope.filterExpression) {
+                                    $scope.filterResult = Nodes.query({
+                                        filter: 'name:.*' + $scope.filterExpression + '.*'
+                                    }, function(data) {
+                                        if (data && data.length > 0) {
+                                            $scope.filterClass['has-error'] = false;
+                                            $scope.filterClass['has-success'] = true;
+                                            $scope.filterIcon['glyphicon-ok'] = true;
+                                            $scope.filterIcon['glyphicon-warning-sign'] = false;
+                                        } else {
+                                            $scope.filterClass['has-error'] = true;
+                                            $scope.filterClass['has-success'] = false;
+                                            $scope.filterIcon['glyphicon-ok'] = false;
+                                            $scope.filterIcon['glyphicon-warning-sign'] = true;
+                                        }
+                                    });
+                                } else {
+                                    $scope.filterResult = [];
+                                    $scope.filterClass['has-error'] = false;
+                                    $scope.filterClass['has-success'] = false;
+                                    $scope.filterIcon['glyphicon-ok'] = false;
+                                    $scope.filterIcon['glyphicon-warning-sign'] = false;
+                                }
                             }
-                        });
+                            AngularTools.safeApply($scope);
+                        }, false);
                     },
                     replace: true,
                     scope: {
